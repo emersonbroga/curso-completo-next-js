554509be983c91ce8f87f4544d6516f285a4a70f
Emerson Broga <git@emersonbroga.com>
2024-02-11 09:26:21 -0300

    Added cn helper.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,42 @@
 import { FaceHappyIcon, GamepadIcon, HomeIcon, PrizeIcon, RouteIcon } from "@/components";
+import { cn } from "@/helpers/cn";
 
-const NavbarList = ({ children, className }) => {
-  return <ul className={`my-4 border-t border-indigo-400/20 hover:border-indigo-400/40 ${className}`}>{children}</ul>;
+type NavbarProps = React.ComponentProps<"nav">;
+type NavbarListProps = React.ComponentProps<"ul">;
+type NavbarListItemProps = React.ComponentProps<"li">;
+
+const NavbarList = ({ children, className, ...props }: NavbarListProps) => {
+  return (
+    <ul className={cn("my-4 border-t border-indigo-400/20 hover:border-indigo-400/40", className)} {...props}>
+      {children}
+    </ul>
+  );
 };
-const NavbarListItem = ({ children, className }) => {
+
+const NavbarListItem = ({ children, className, ...props }: NavbarListItemProps) => {
   return (
     <li
-      className={`my-2 rounded-lg bg-transparent p-2 hover:bg-indigo-400/40 hover:text-slate-100 cursor-pointer flex gap-2 items-center ${className}`}
+      className={cn(
+        "my-2 rounded-lg bg-transparent p-2 hover:bg-indigo-400/40 hover:text-slate-100 cursor-pointer flex gap-2 items-center",
+        className
+      )}
+      {...props}
     >
       {children}
     </li>
   );
 };
 
-export const Navbar = () => {
+export const Navbar = ({ className, ...props }: NavbarProps) => {
   return (
-    <nav className="flex h-screen flex-col bg-slate-900 border-r border-indigo-400/20 hover:border-indigo-400/40 w-72 p-2 text-slate-300">
-      <div className="flex items-center justify-center  my-4">
+    <nav
+      className={cn(
+        "flex h-screen flex-col bg-slate-900 border-r border-indigo-400/20 hover:border-indigo-400/40 w-72 p-2 text-slate-300",
+        className
+      )}
+      {...props}
+    >
+      <div className="flex items-center justify-center my-4">
         <img
           src="https://emersonbroga.com/e/assets/emersonbroga-logo-name-pink.png"
           alt="Logo EmersonBrogaDev"
