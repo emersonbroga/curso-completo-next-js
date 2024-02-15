89fac5f611aa7eb932fc0d0da7bb73917403915a
Emerson Broga <git@emersonbroga.com>
2024-02-15 11:10:56 -0300

    Server side validation

--- src/app/auth/sign-up/SignUpForm.tsx
+++ src/app/auth/sign-up/SignUpForm.tsx
@@ -2,18 +2,27 @@
 
 import { SubmitButton, TextInput } from "@/components";
 import { useFormState } from "react-dom";
-import { handleSignUpForm } from "./actions";
+import { handleSignUpForm, type SignUpState } from "./actions";
+
+const initialState: SignUpState = {
+  isValid: undefined,
+  errors: { name: undefined, email: undefined, password: undefined, passwordConfirmation: undefined },
+};
 
 export default function SignUpForm() {
-  const [state, formAction] = useFormState(handleSignUpForm, {});
-  console.log("🟨 *** ~ state:", state);
+  const [state, formAction] = useFormState(handleSignUpForm, initialState);
 
   return (
     <form action={formAction}>
-      <TextInput name="name" label="Name" />
-      <TextInput name="email" label="E-mail" inputMode="email" />
-      <TextInput name="password" label="Password" type="password" />
-      <TextInput name="passwordConfirmation" label="Password confirmation" type="password" />
+      <TextInput name="name" label="Name" error={state.errors.name} />
+      <TextInput name="email" label="E-mail" inputMode="email" error={state.errors.email} />
+      <TextInput name="password" label="Password" type="password" error={state.errors.password} />
+      <TextInput
+        name="passwordConfirmation"
+        label="Password confirmation"
+        type="password"
+        error={state.errors.passwordConfirmation}
+      />
 
       <SubmitButton label="Create account" />
     </form>
--- src/app/auth/sign-up/actions.ts
+++ src/app/auth/sign-up/actions.ts
@@ -2,8 +2,63 @@
 
 import UsersService from "@/services/Users";
 
+export type SignUpError = {
+  name?: string;
+  email?: string;
+  password?: string;
+  passwordConfirmation?: string;
+};
+
+export type SignUpState = {
+  isValid?: boolean;
+  errors: SignUpError;
+};
+
+const validateSignUpForm = (formData: FormData) => {
+  const errors: SignUpError = {
+    name: undefined,
+    email: undefined,
+    password: undefined,
+    passwordConfirmation: undefined,
+  };
+
+  const name = String(formData.get("name"));
+  const email = String(formData.get("email"));
+  const password = String(formData.get("password"));
+  const passwordConfirmation = String(formData.get("passwordConfirmation"));
+
+  try {
+    if (!name) {
+      errors.name = "Name is required";
+    }
+
+    if (!email) {
+      errors.email = "Email is required";
+    }
+
+    if (!email.includes("@")) {
+      errors.email = "Email is not valid";
+    }
+
+    if (password.length < 10) {
+      errors.password = "Password should have 10 chars";
+    }
+
+    if (!password || password !== passwordConfirmation) {
+      errors.passwordConfirmation = "Password confirmation doesn't match";
+    }
+    const isValid = Object.values(errors).every((v) => v === undefined);
+    return { isValid, errors };
+  } catch (error) {
+    return { isValid: false, errors };
+  }
+};
+
 export const handleSignUpForm = async (prevState: any, formData: FormData) => {
-  console.log("*** handleSignUpForm ", Object.fromEntries(formData));
+  const validation = validateSignUpForm(formData);
+  if (!validation.isValid) {
+    return { ...prevState, ...validation };
+  }
 
   const data = {
     name: String(formData.get("name")),
@@ -12,7 +67,6 @@ export const handleSignUpForm = async (prevState: any, formData: FormData) => {
   };
 
   const record = await UsersService.signUp(data);
-  console.log("🟨 *** ~ record:", record);
 
-  return {};
+  return { ...prevState, isValid: true };
 };
